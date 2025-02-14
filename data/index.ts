12e486872ee05fc83687c63849d9e3dadab36938
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-14 07:56:58 +0100

    add bentogrid with random data

---
+++ data/index.ts
@@ -0,0 +1,23 @@
+export const gridItems = [
+  {
+    id: 1,
+    title: "Mi impegno molto nella comunicazione tra colleghi ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Mi impegno molto nella comunicazione tra colleghi ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "",
+    spareImg: "",
+  },
+  
+];
