376db4af0b25a0976e69745ffcab1d92218a481e
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-20 15:42:36 +0100

    fix text hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,14 +31,14 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Jonathan Ultini
           </p>
           <TextGenerateEffect
             className='text-center text-[40px] md:text-5xl lg:text-6xl'
-            words='Transforming Concepts into Seamless User Experiences'
+            words='Inizio il mio percorso nello sviluppo web creando esperienze utente fluide e accessibili.'
           />
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-            Ciao, sono Jonathan uno sviluppatore web
+            Ciao! Sono Jonathan, un junior web developer appassionato di sviluppo
           </p>
 
           <a href="#projects">
