63ac4f7e0000ba51841c0eeaaef3f018d07944ae
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-20 12:09:54 +0100

    add footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
@@ -15,9 +16,10 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients />
-        <Experience />
-        <Approach />
+        {/* <Clients /> */}
+        {/* <Experience /> */}
+        {/* <Approach /> */}
+        <Footer />
       </div>
     </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -182,14 +182,14 @@ export const workExperience = [
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
+    className: "md:col-span-2", 
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
+    className: "md:col-span-2",
     thumbnail: "/exp3.svg",
   },
   {
@@ -205,13 +205,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
+    link: "https://github.com/Jonathan-Ultini"
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/jonathan-ultini"
   },
 ];
