02877ebb862c18089bbb4da3ae45e3e0a340e32f
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-20 22:43:06 +0100

    fix letter

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: " A full-stack project usando Node.js, Express.js, PostgreSQL, Prisma, and Docker.",
+    title: " A full-stack project usando Node.js, Express.js, PostgreSQL, Prisma, e Docker.",
     description: "In fase di sviluppo:",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
