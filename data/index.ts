dc6e0935591aed405ae5d904db8220adc8f36fec
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-20 22:42:27 +0100

    add logo and next project

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Sviluppo sistemi backend solidi con autenticazione JWT, operazioni CRUD su database e API REST, utilizzando tecnologie e framework moderni",
+    title: " A full-stack project usando Node.js, Express.js, PostgreSQL, Prisma, and Docker.",
     description: "In fase di sviluppo:",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
